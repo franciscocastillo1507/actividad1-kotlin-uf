61f6ebe814047e54818a3398ce7151d6e5ea2afd
franciscocastillo1507 <54192394+franciscocastillo1507@users.noreply.github.com>
2019-08-21 21:24:42 -0500

    Create Student2.kts

---
+++ Student2.kts
@@ -0,0 +1,18 @@
+//No se porque no me dejo subirlo lo de la actividad
+
+
+package com.test.actividad1.Actividad1
+
+/*
+* DOC APOYO: https://kotlinlang.org/docs/reference/basic-types.html
+* KOTLIN KOANS: https://play.kotlinlang.org/koans/overview
+* */
+
+println("Hola Mundo")
+println("uso de kotlin")
+val nombre = "Paco"
+println(nombre)
+var x=12
+var y=8
+val edad=x+y
+println("Mi edad es "+edad+" años")
