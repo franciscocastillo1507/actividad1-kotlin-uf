da0f5c534554cb1147fd54cc1c7a6ed6fe9a2f73
gimape07 <a01702312@itesm.mx>
2019-08-21 21:03:13 -0500

    final version

--- app/src/main/java/com/test/actividad1/Actividad1/Student3.kts
+++ app/src/main/java/com/test/actividad1/Actividad1/Student3.kts
@@ -10,15 +10,28 @@ var caso1:ArrayList<Int> = arrayListOf(3,4,2,6,7) //2
 var caso2:ArrayList<Int> = arrayListOf(1) //0
 var caso3:ArrayList<Int> = arrayListOf(3,4,2,3,7) //2
 var caso4:ArrayList<Int> = arrayListOf(44,20,114,45,68,142,70,12,256) //2
-var max = 2
+var max = 300
 
 fun enterosPositivos(input:ArrayList<Int>):Int{
     println("Tamano de la entrada: " + input.size)
     var hash = ArrayList<Int> (max)
     for (x in 0 until max)
         hash.add(0)
-    for (i in hash)
-        println(i)
+    for(x in 0 until input.size)
+        hash[input[x]] = hash[input[x]].inc()
+    var res = 0
+    for (x in 0 until input.size) {
+        var y = input[x]
+        for (z in 0 until input.size){
+            if (input[z] < y){
+                res += hash[y-input[z]]
+                if ((input[z] == y/2) && (y%2 == 0) && (hash[y-input[z]] > 0))
+                    res-=1
+            }
+        }
+    }
+    res /= 2
+    println("Resultado: " +  res)
     return 0
 }
 println("Caso 1:")
